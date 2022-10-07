92068143775502f48f659f7f15acfc4f5ba3b4e3
MAThatcher <35704138+mat-18@users.noreply.github.com>
2022-10-07 14:00:09 -0500

    .
    .

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,12 @@ const axios = require("axios");
 router.get(`/chat`, (req, res, next) => {
   const input = { message, sender, character, radius, location } = req.query;
   const content = `**${input.sender}** acting as **${input.character}** ${input.radius}s: \`${input.message}\``;
-
-
-
-
-  
+  const key1 = 'https://discord.com/api/';
+  const key2 = 'webhooks/1028017497380765716/';
+  const key3 = 'ESX597kjHHa8Uxy_oJskGAu97BoLgcL73';
+  const key4 = '__P4elP7_61lS5e7rd5kly-lsgDKI4OyruV';
   axios.post(
-    "https://discord.com/api/webhooks/1021164664438141038/znK1W46R_NXKUBtMZPBlGYg2ABKIiV8Rfi20hL90zGgBUusZujH0UBieEiOfx5sto8wE",
+    `${key1}${key2}${key3}${key4}`,
     {
       content: content,
     }
@@ -42,11 +41,13 @@ router.get(`/chat`, (req, res, next) => {
 
 router.get(`/log`, (req, res, next) => {
   const {date, steamId, charName, actName, eventId, eventCategory, eventType, params } = req.query;
-
   const content = `\`${date}\`: **${charName}** as **${actName}** | ${eventCategory} | ${eventType} | \`${params}\``;
-
+  const key1 = 'https://discord.com/api/';
+  const key2 = 'webhooks/1028016792834150400/';
+  const key3 = 'TkmUcCajTqJwY8hhAUN7ojbTqHWD1';
+  const key4 = 'tsmS8qdaeW67Dj3mgKn9eZeXycEr_5Gr7yGTGnG';
   axios.post(
-    "https://discord.com/api/webhooks/1021632130515742811/yrAkRtaA9cqDqWZ5fk-P2ePwD2BgrBofWrF9rlNSJ0A17zE-Hr0wR_buN1cI4RauEAPx",
+    `${key1}${key2}${key3}${key4}`,
     {
       content: content,
     }
